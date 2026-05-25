10e89b9f69af665767f7e3ef68874bdc516d7155
ang-jason <85982975+ang-jason@users.noreply.github.com>
2026-05-25 16:29:55 +0800

    Update run.py

--- run.py
+++ run.py
@@ -9,4 +9,4 @@ load_dotenv()
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("api.main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("api.main:app", host="0.0.0.0", port=8000, reload=False)
